933b648ee41f7e800059de863843a9560b22e9c7
Alicia G <alicia.gonzalez.90@gmail.com>
2012-11-18 15:47:02 -0600

    add/edit/drop article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :status, :name
+  attr_accessible :status, :name, :descripcionAbreviada, :descripcionCompleta, :ubicacion
   # attr_accessible :title, :body
   belongs_to :user
 end
